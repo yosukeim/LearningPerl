175d8353894b971952df943c2256d10b8115cb4f
yosuke <yosukeim@gmail.com>
2009-08-12 01:35:43 +0900

    one liner

--- fizzbuzz.pl
+++ fizzbuzz.pl
@@ -7,15 +7,7 @@ use warnings;
 # 3の倍数の場合は「Fizz」、5の倍数は「Buzz」、15の倍数は「Fizz Buzz」と表示するようなプログラムを書く
 
 for (my $xx = 1; $xx <= 100; $xx++) {
-	if (not $xx % 15) {
-		print "Fizz Buzz\n";
-	} elsif (not $xx % 3) {
-		print "Fizz\n";
-	} elsif (not $xx % 5) {
-		print "Buzz\n";
-	} else {
-		print "$xx\n";
-	}
+	$xx % 15 ? $xx % 3 ? $xx % 5 ? print "$xx\n" : print "Buzz\n" : print "Fizz\n" : print "Fizz Buzz\n" ;
 }
 
 
