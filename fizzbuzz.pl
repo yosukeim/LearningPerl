cc174a8be466c077a7b2885d45d797a3a79bc2bc
yosuke <yosukeim@gmail.com>
2009-08-13 00:58:59 +0900

    more short

--- fizzbuzz.pl
+++ fizzbuzz.pl
@@ -6,15 +6,15 @@ use warnings;
 # 1 から 1000 まで順に数を数えていって、
 # 3の倍数の場合は「Fizz」、5の倍数は「Buzz」、15の倍数は「Fizz Buzz」と表示するようなプログラムを書く
 
-for (my $xx = 1; $xx <= 1000; $xx++) {
-	if (not $xx % 15) {
+for (1..100) {
+	if (not $_ % 15) {
 		print "Fizz Buzz\n";
-	} elsif (not $xx % 3) {
+	} elsif (not $_ % 3) {
 		print "Fizz\n";
-	} elsif (not $xx % 5) {
+	} elsif (not $_ % 5) {
 		print "Buzz\n";
 	} else {
-		print "$xx\n";
+		print "$_\n";
 	}
 }
 
