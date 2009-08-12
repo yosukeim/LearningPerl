72d6b26b233e0dd00ad6392bc2c8ad6afdae2789
yosuke <yosukeim@gmail.com>
2009-08-13 01:04:57 +0900

    bug fix

--- fizzbuzz.pl
+++ fizzbuzz.pl
@@ -6,7 +6,7 @@ use warnings;
 # 1 から 1000 まで順に数を数えていって、
 # 3の倍数の場合は「Fizz」、5の倍数は「Buzz」、15の倍数は「Fizz Buzz」と表示するようなプログラムを書く
 
-for (1..100) {
+for (1..1000) {
 	if (not $_ % 15) {
 		print "Fizz Buzz\n";
 	} elsif (not $_ % 3) {
