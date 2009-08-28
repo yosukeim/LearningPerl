417996a38bb69d75f48b12eedc46f42b5684f91b
yosuke <yosukeim@gmail.com>
2009-08-29 03:46:23 +0900

    update date.pl

--- date.pl
+++ date.pl
@@ -3,15 +3,20 @@ use strict;
 use warnings;
 
 
-# 今日からの○日後が○年○月○日かを表示してくれるプログラムを書く
+# 今日から○日後が、○年○月○日かを表示してくれるプログラム
 
-my $timestamp = time;
-my ($sec, $min, $hour, $day, $mon, $year, $wday, $yday, $isdst) = localtime $timestamp;
+my $now = time;
+
+print "今日から何日後の日付が知りたいですか？数字のみで入力してください。\n";
+chomp(my $later = <STDIN>);
+
+my $newtimestamp = $now + ($later * 24 * 60 * 60);
+my ($sec, $min, $hour, $day, $mon, $year, $wday, $yday, $isdst) = localtime $newtimestamp;
 
 $year += 1900;
 $mon += 1;
-my @week = ('Sun', 'Mon', 'Tue', 'Wed', 'Thr', 'Fri', 'Sat');
+my @week = ('日', '月', '火', '水', '木', '金', '土');
 
-print "$year/$mon/$day $week[$wday]\n";
+print "今日から $later日後は $year年$mon月$day日 $week[$wday]曜日です。\n";
 
 
